7edd6835830fd18aeba3ba6735dce28bba0483b7
Yoshie TAKEDA <takedayoshie@YT-MacBook-Air.local>
2017-01-21 14:26:15 +0900

    20170121-1426
    形だけ作りました

---
+++ WevesGame/Assets/GameManeger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour {
+
+// ステートの定義
+	public enum GameState{
+		None = -1,
+		Start,
+		Play,
+		Clear,
+		Gameover,
+	}
+	GameState state = GameState.None;
+
+	void Start () {
+		state = GameState.Start;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// ステート遷移+実行フェイズ
+		switch(state){
+			case GameState.Start:
+			break;
+			case GameState.Play:
+			break;
+			case GameState.Clear:
+			break;
+			case GameState.Gameover:
+			break;
+			}
+		// 実行フェイズ
+		switch(state){
+			case GameState.Start:
+			break;
+			case GameState.Play:
+			break;
+			case GameState.Clear:
+			break;
+			case GameState.Gameover:
+			break;
+		}
+	}
+}
