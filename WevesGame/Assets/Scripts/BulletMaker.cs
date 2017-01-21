0efaf71851241feaa3fab2d89dd540f87ac66aeb
Yoshie TAKEDA <takedayoshie@YT-MacBook-Air.local>
2017-01-22 01:04:57 +0900

    0104
    Scripts

---
+++ WevesGame/Assets/Scripts/BulletMaker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BulletMaker : MonoBehaviour {
+
+    private Vector3 bulletPosition;
+    [SerializeField]float speed;
+    private bool isReady;
+
+
+
+    // Use this for initialization
+    void Start () {
+        isReady = true;
+		
+	}
+
+
+
+    // Update is called once per frame
+    void Update() {
+        //マウスの左ボタンの判定を取得する。
+        if (Input.GetMouseButton(0))
+        {
+            //左ボタンが離れるタイミングを取得
+            if (Input.GetMouseButton(0) && isReady == true)
+            {
+                isReady = false;
+
+                //弾の生成位置を砲身に同期
+                bulletPosition = this.transform.position;
+
+                //離れたら弾のprefabを取得して生成
+                GameObject prefab = (GameObject)Resources.Load("Prefabs/cannonball");
+                Instantiate(prefab, bulletPosition, Quaternion.identity);
+
+                Invoke("StandBy" , 1.5f);
+            }
+        }
+
+    }
+        void StandBy () {
+        isReady = true;
+    }
+}
